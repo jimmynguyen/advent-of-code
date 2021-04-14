60624ddadba026967109c347bc6311d50e2085eb
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-13 23:23:13 -0400

    completed 2016 day 05 challenge

---
+++ 2016/tests/test_day05.py
@@ -0,0 +1,13 @@
+from day05 import Day05
+import unittest
+
+
+class TestDay05(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input = "abc"
+
+    def test_part1(self):
+        self.assertEqual(Day05.solve_part1(self.input),"18f47a30")
+
+    def test_part2(self):
+        self.assertEqual(Day05.solve_part2(self.input),"05ace8e3")
