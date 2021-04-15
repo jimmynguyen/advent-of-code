d0ad947cc0961a88f7078f9cd6d082c784dfb1b0
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-15 01:47:01 -0400

    completed 2016 day 11 challenge

---
+++ 2016/tests/test_day11.py
@@ -0,0 +1,10 @@
+from day11 import Day11
+import unittest
+
+
+class TestDay11(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input = Day11().read_file("tests/test_day11.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day11.solve_part1(self.input),11)
