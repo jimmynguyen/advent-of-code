5b895d99d544fd2ca2780994b81d398d95e0dc8a
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-18 01:04:48 -0400

    completed 2016 day 17 challenge

---
+++ 2016/tests/test_day17.py
@@ -0,0 +1,14 @@
+from day17 import Day17
+import unittest
+
+
+class TestDay17(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(Day17.solve_part1("ihgpwlah"),"DDRRRD")
+        self.assertEqual(Day17.solve_part1("kglvqrro"),"DDUDRLRRUDRD")
+        self.assertEqual(Day17.solve_part1("ulqzkmiv"),"DRURDRUDDLLDLUURRDULRLDUUDDDRR")
+
+    def test_part2(self):
+        self.assertEqual(Day17.solve_part2("ihgpwlah"),370)
+        self.assertEqual(Day17.solve_part2("kglvqrro"),492)
+        self.assertEqual(Day17.solve_part2("ulqzkmiv"),830)
