a76eb9d414496ea93b66dd8225565786300c05d3
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-18 03:42:10 -0400

    completed 2016 day 20 challenge

---
+++ 2016/tests/test_day20.py
@@ -0,0 +1,13 @@
+from day20 import Day20
+import unittest
+
+
+class TestDay20(unittest.TestCase):
+    def setUp(self):
+        self.input = Day20().read_file("tests/test_day20.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day20.solve_part1(self.input),3)
+
+    def test_part2(self):
+        self.assertEqual(Day20.solve_part2(self.input,10),2)
