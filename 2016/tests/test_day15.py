09e3dcde4ab4beb329252c54390215ea06c0afbb
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-17 23:31:06 -0400

    completed 2016 day 15 challenge

---
+++ 2016/tests/test_day15.py
@@ -0,0 +1,10 @@
+from day15 import Day15
+import unittest
+
+
+class TestDay15(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input = Day15().read_file("tests/test_day15.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day15.solve_part1(self.input),5)
