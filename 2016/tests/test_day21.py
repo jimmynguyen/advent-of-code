5d20c2b608391d0e486987f644c242dc0ffdc3e3
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-20 16:49:14 -0400

    completed 2016 day 21 challenge

---
+++ 2016/tests/test_day21.py
@@ -0,0 +1,13 @@
+from day21 import Day21
+import unittest
+
+
+class TestDay21(unittest.TestCase):
+    def setUp(self):
+        self.input = Day21().read_file("tests/test_day21.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day21.solve_part1(self.input,"abcde"),"decab")
+
+    def test_part2(self):
+        self.assertEqual(Day21.solve_part2(self.input,"decab"),"abcde")
