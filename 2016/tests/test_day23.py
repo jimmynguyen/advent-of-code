c4ec28af2a42ec2efe37a6d0a2ad5ee0e87f88e3
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-21 14:36:54 -0400

    completed 2016 day 23 challenge

---
+++ 2016/tests/test_day23.py
@@ -0,0 +1,10 @@
+from day23 import Day23
+import unittest
+
+
+class TestDay23(unittest.TestCase):
+    def setUp(self):
+        self.input = Day23().read_file("tests/test_day23.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day23.solve_part1(self.input,simplify=False),3)
