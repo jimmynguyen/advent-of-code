e0800c219fac40ec746e818516c7a84a181d739a
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-21 16:21:24 -0400

    completed 2017 day 01 challenge

---
+++ 2017/day01.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2017/day/1
+from challenge import Challenge
+
+
+class Day01(Challenge):
+    @staticmethod
+    def solve_part1(captcha,n=1):
+        return sum(int(x) for x,y in zip(captcha,captcha[n:]+captcha[0:n]) if x == y)
+
+    @staticmethod
+    def solve_part2(captcha):
+        return Day01.solve_part1(captcha,n=len(captcha)//2)
+
+
+if __name__ == "__main__":
+    Day01().solve_all()
