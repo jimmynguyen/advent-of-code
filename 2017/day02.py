7bc165c8cc9714c0f9a9be86d21744e7aa281531
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-21 16:30:08 -0400

    completed 2017 day 02 challenge

---
+++ 2017/day02.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2017/day/2
+from challenge import Challenge
+from itertools import combinations
+
+
+class Day02(Challenge):
+    def read_file(self,filename):
+        return [list(map(int,x.split())) for x in super().read_file(filename)]
+
+    @staticmethod
+    def solve_part1(data):
+        return sum(max(x)-min(x) for x in data)
+
+    @staticmethod
+    def solve_part2(data):
+        return sum(max(y)//min(y) for x in data for y in combinations(x,2) if max(y)%min(y) == 0)
+
+
+if __name__ == "__main__":
+    Day02().solve_all()
