f68b4c7ad6f900499e943d7f6f0d76abe0b0b0da
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-24 01:31:30 -0400

    completed 2017 day 03 challenge

---
+++ 2017/tests/test_day03.py
@@ -0,0 +1,10 @@
+from day03 import Day03
+import unittest
+
+
+class TestDay03(unittest.TestCase):
+    def test_part1(self):
+        inputs = [1,12,23,1024]
+        outputs = [0,3,2,31]
+        for input,output in zip(inputs,outputs):
+            self.assertEqual(Day03.solve_part1(input),output)
