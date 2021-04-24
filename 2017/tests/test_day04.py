1314f3f744c937d4b5102c2e5abc422b39d62758
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-24 01:42:23 -0400

    completed 2017 day 04 challenge

---
+++ 2017/tests/test_day04.py
@@ -0,0 +1,10 @@
+from day04 import Day04
+import unittest
+
+
+class TestDay04(unittest.TestCase):
+    def setUp(self):
+        self.input = Day04().read_file("tests/test_day04.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day04.solve_part1(self.input),2)
