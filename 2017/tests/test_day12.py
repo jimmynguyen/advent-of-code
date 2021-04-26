9b46cd50bf22ce1b4520138d7d40d0e19261a53e
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-04-26 14:04:41 -0400

    completed 2017 day 12 challenge

---
+++ 2017/tests/test_day12.py
@@ -0,0 +1,17 @@
+from day12 import Day12
+import unittest
+
+
+class TestDay12(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input = Day12().read_file("tests/test_day12.txt")
+
+    def test_part1(self) -> None:
+        output = 6
+        actual = Day12.solve_part1(self.input)
+        self.assertEqual(actual, output, f"input={self.input}, expected={output}, actual={actual}")
+
+    def test_part2(self) -> None:
+        output = 2
+        actual = Day12.solve_part2(self.input)
+        self.assertEqual(actual, output, f"input={self.input}, expected={output}, actual={actual}")
