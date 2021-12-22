e9ced7902adddb85a98efe48a6ee43a4f0287f8d
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-12-22 15:27:40 -0500

    Complete 2021 day 07 challenge

---
+++ 2021/tests/test_day07.py
@@ -0,0 +1,13 @@
+from day07 import Day07
+import unittest
+
+
+class TestDay07(unittest.TestCase):
+    def setUp(self):
+        self.input = Day07().read_file("tests/test_day07.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day07.solve_part1(self.input), 37)
+
+    def test_part2(self):
+        self.assertEqual(Day07.solve_part2(self.input), 168)
