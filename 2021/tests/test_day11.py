bc6acb6652676ee5af633856e0cced8ecdb65a34
jimmynguyen <jimmy.nguyen@gatech.edu>
2021-12-24 00:05:05 -0500

    Complete 2021 day 11 challenge

---
+++ 2021/tests/test_day11.py
@@ -0,0 +1,16 @@
+from day11 import Day11
+import unittest
+
+
+class TestDay11(unittest.TestCase):
+    def setUp(self):
+        self.input = Day11().read_file("tests/test_day11.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day11.solve_part1(self.input), 1656)
+
+    def test_part2(self):
+        self.assertEqual(Day11.solve_part2(self.input), 195)
+
+if __name__ == "__main__":
+    unittest.main()
