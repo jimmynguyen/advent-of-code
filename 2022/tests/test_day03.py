9530213a614c1cf2b625ef7864286b05b4579507
jimmynguyen <jimmy.nguyen@gatech.edu>
2022-12-03 07:57:08 -0600

    Complete 2022 day 03 challenge

---
+++ 2022/tests/test_day03.py
@@ -0,0 +1,16 @@
+from day03 import Day03
+import unittest
+
+
+class TestDay03(unittest.TestCase):
+    def setUp(self):
+        self.input = Day03().read_file("tests/test_day03.txt")
+
+    def test_part1(self):
+        self.assertEqual(Day03.solve_part1(self.input), 157)
+
+    def test_part2(self):
+        self.assertEqual(Day03.solve_part2(self.input), 70)
+
+if __name__ == "__main__":
+    unittest.main()
