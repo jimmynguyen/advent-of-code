b498de78a8c894a856f3d129a00d11321f293dea
jimmynguyen <jimmy.nguyen@gatech.edu>
2024-11-30 14:57:52 -0600

    feat: complete 2018 day01 challenge

---
+++ 2018/day01/day01.go
@@ -0,0 +1,63 @@
+// https://adventofcode.com/2018/day/1
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func handleError(
+	message string,
+	err error,
+) {
+	if err != nil {
+		panic(fmt.Sprintf(message+": %v", err))
+	}
+}
+
+func part1(lines []string) {
+	result := 0
+	for _, line := range lines {
+		num, err := strconv.Atoi(line[1:])
+		handleError("Failed to convert string to number", err)
+		if line[0] == '+' {
+			result += num
+		} else {
+			result -= num
+		}
+	}
+	fmt.Println("Part 1 result:", result)
+}
+
+func part2(lines []string) {
+	visited := map[int]bool{0: true}
+	result := 0
+	for true {
+		for _, line := range lines {
+			num, err := strconv.Atoi(line[1:])
+			handleError("Failed to convert string to number", err)
+			if line[0] == '+' {
+				result += num
+			} else {
+				result -= num
+			}
+			_, ok := visited[result]
+			if ok {
+				fmt.Println("Part 2 result:", result)
+				return
+			}
+			visited[result] = true
+		}
+	}
+}
+
+func main() {
+	filename := "day01.txt"
+	fileBytes, err := os.ReadFile(filename)
+	handleError("Failed to read file", err)
+	lines := strings.Split(string(fileBytes), "\n")
+	part1(lines)
+	part2(lines)
+}
