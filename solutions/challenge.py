b780cc366fb46115c0c773fb583ea02977bb5da0
jimmynguyen <jimmy.nguyen@gatech.edu>
2024-12-09 19:10:17 -0600

    feat: update file path

--- solutions/challenge.py
+++ solutions/challenge.py
@@ -23,6 +23,6 @@ class Challenge:
         classname = self.__class__.__name__.lower()
         year = os.path.basename(os.getcwd())
         print(f"\n{year} {classname} challenge")
-        filename = f"{classname}.txt"
+        filename = os.path.join(os.path.dirname(__file__), "inputs", year, f"{classname}.txt")
         print("part 1 answer:",self.solve_part1(self.read_file(filename) if input is None else input))
         print("part 2 answer:",self.solve_part2(self.read_file(filename) if input is None else input))
