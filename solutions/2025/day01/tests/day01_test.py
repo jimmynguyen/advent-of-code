7a559311a4d8288d59a8c759530545b31d2fff0b
jimmynguyen <jimmy.nguyen@gatech.edu>
2025-12-02 15:54:16 -0600

    chore: restructure 2025 day01 challenge solution

--- solutions/2025/day01/tests/day01_test.py
+++ solutions/2025/day01/tests/day01_test.py
@@ -1,14 +1,14 @@
-from day01 import Day01
+from day01.day01 import Day01
 import unittest
 
 
 class TestDay01(unittest.TestCase):
     def test_part1(self):
-        input = Day01().read_file("tests/test_day01.txt")
+        input = Day01().read_file("day01/tests/day01.test.txt")
         self.assertEqual(Day01.solve_part1(input),3)
 
     def test_part2(self):
-        input = Day01().read_file("tests/test_day01.txt")
+        input = Day01().read_file("day01/tests/day01.test.txt")
         self.assertEqual(Day01.solve_part2(input),6)
 
     def test_part2_2(self):
@@ -30,6 +30,7 @@ class TestDay01(unittest.TestCase):
     def test_part2_6(self):
         input = [-633]
         self.assertEqual(Day01.solve_part2(input,1),7)
+
 
 if __name__ == "__main__":
     unittest.main()
